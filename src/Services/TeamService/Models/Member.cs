0ddade268e3ee155b5540258de57954816f6099c
He.Zhang <jerald.z@outlook.com>
2021-07-24 12:10:23 +0800

    tests: add models for teams controller

---
+++ src/Services/TeamService/Models/Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamService.Models
+{
+    public class Member
+    {
+        public Member()
+        {
+
+        }
+
+        public Member(Guid guid)
+        {
+            ID = guid;
+        }
+
+        public Member(string firstName, string lastName, Guid guid) : this(guid)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Guid ID { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ src/Services/TeamService/Models/Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamService.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            Members = new List<Member>();
+        }
+
+        public Team(string name) : this()
+        {
+            Name = name;
+        }
+
+        public Team(string name, Guid guid) : this(name)
+        {
+            ID = guid;
+        }
+
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+        public ICollection<Member> Members { get; set; }
+    }
+}
